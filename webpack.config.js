b486202519b2ccf4756be8461e8deb0d34c62392
zakuni <kunio038@gmail.com>
2017-10-08 17:47:04 +0900

    add babel and react environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/client.js',
+  entry: './src/client.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   },
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
