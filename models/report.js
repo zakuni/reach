f80de766c5b855e5210027663f5e2d70cbc030fd
zakuni <kunio038@gmail.com>
2017-10-22 22:22:19 +0900

    add timestamps to models

--- models/report.js
+++ models/report.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const reportSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Report', reportSchema);
 
--- models/user.js
+++ models/user.js
@@ -3,6 +3,6 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   reports: [{ type: Schema.Types.ObjectId, ref: 'Report'}]
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
