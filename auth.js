9083093d00c49981fa5767de6ddab4e214a0fc75
zakuni <kunio038@gmail.com>
2017-11-07 19:56:37 +0900

    divide auth.js

---
+++ auth.js
@@ -0,0 +1,24 @@
+const passport = require('koa-passport');
+const User = require('./models/user');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/google/callback'
+},
+function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}));
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const Koa = require('koa');
 const app = new Koa();
-const User = require('./models/user');
 
 app.use(logger());
 
@@ -28,30 +27,8 @@ app.use(views(path.resolve(__dirname, 'views'), {
 }));
 app.use(require('koa-static')(path.resolve(__dirname, 'dist')));
 
+require('./auth');
 const passport = require('koa-passport');
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/google/callback'
-},
-function(accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    return done(err, user);
-  });
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
