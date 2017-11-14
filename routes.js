ffd57efa21dd26e2bd507e08b5351e6f40b77b9e
zakuni <kunio038@gmail.com>
2017-11-14 22:00:46 +0900

    refactor

--- routes.js
+++ routes.js
@@ -44,25 +44,24 @@ router.get('/', async function (ctx, next) {
 
 // eslint-disable-next-line no-unused-vars
 router.get('/index', async function (ctx, next) {
-  let content;
-  let css;
   if (ctx.isUnauthenticated()) {
     return ctx.redirect('/');
-  } else {
-    content = ReactDOMServer.renderToString(
-      <StaticRouter
-        location={ctx.request.URL}
-        context={ctx}
-      >
-        <JssProvider registry={sheetsRegistry} jss={jss}>
-          <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
-            <App/>
-          </MuiThemeProvider>
-        </JssProvider>
-      </StaticRouter>
-    );
-    css = sheetsRegistry.toString();
   }
+
+  const content = ReactDOMServer.renderToString(
+    <StaticRouter
+      location={ctx.request.URL}
+      context={ctx}
+    >
+      <JssProvider registry={sheetsRegistry} jss={jss}>
+        <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+          <App/>
+        </MuiThemeProvider>
+      </JssProvider>
+    </StaticRouter>
+  );
+  const css = sheetsRegistry.toString();
+
   await ctx.render('index', {
     content: content,
     css: css
