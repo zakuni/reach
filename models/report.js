7218d7390b20c2fed0a491880b605d7ea00f2042
zakuni <kunio038@gmail.com>
2017-11-24 21:43:57 +0900

    add validation to Report
    unique title per author

--- models/report.js
+++ models/report.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema;
 
 const reportSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
+  title: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        this.model.count({ author: this.author, title: v }) === 0;
+      },
+      message: '{VALUE} already exists'
+    }
+  },
   content: String
 }, { timestamps: true });
 
